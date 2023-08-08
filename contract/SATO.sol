f543a2e436bdae7acb30228d12f236cf7b28f4e0
Sato <141697508+satothedog@users.noreply.github.com>
2023-08-08 12:43:01 +0900

    Update SATO.sol

--- contract/SATO.sol
+++ contract/SATO.sol
@@ -1,1 +1,12 @@
+pragma solidity ^0.8.0;
+
+contract SATO is ERC20, Ownable {
+    constructor() ERC20("Sato", "SATO") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function burn(uint256 amount) public virtual{
+        _burn(msg.sender, amount);
+    }
+}
 
